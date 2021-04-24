7235f00c8332cae1558b107a9bfee578b322b406
shin224224 <sasasaeshin@gmail.com>
2021-04-24 15:19:45 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'massages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "massages#index"
+  resources :users, only: [:edit, :update]
 end
