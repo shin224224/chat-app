cceac49aa5f5b05f7cd5330a964d40659d13af26
shin224224 <sasasaeshin@gmail.com>
2021-04-26 22:48:47 +0900

    メッセージ送信機能の実装

--- app/controllers/massages_controller.rb
+++ app/controllers/massages_controller.rb
@@ -1,4 +1,25 @@
 class MassagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(paramas[:room_id])
+
   end
+
+
+  def create
+    @room = Room.find(paramas[:room_id])
+    @message = @room.messages.new(message_params)
+    
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+end
+
+private
+
+def message_params
+  paramas.require(:message).permit(:content).merge(user_id: current_user.id)
+end
 end
