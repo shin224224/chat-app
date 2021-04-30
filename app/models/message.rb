b207bedeebbfc41bfde8458b6301af26ba7e78ce
shin224224 <sasasaeshin@gmail.com>
2021-04-30 23:17:23 +0900

    各モデルのテスト

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   has_one_attached :image 
-  varidates :content, presence: true, unless: :was_atacched?
+  validates :content, presence: true, unless: : was_atacched?
 
   def was_attached?
     self.image.was_atacched?
